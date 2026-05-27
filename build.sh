c62d3630a2a47146f63b2732103fbdd93e6c720e
iammuntakim <mdmuntakimamin@gmail.com>
2026-05-27 17:21:22 +0600

    Initial commit of packages

--- build.sh
+++ build.sh
@@ -5,25 +5,23 @@ BUILD_DIR="build$RANDOMID"
 
 mkdir -p "$BUILD_DIR/bin" "$BUILD_DIR/lib"
 
-CGO="./crt0/crt0.ssk"
+CGO_SRC="native/cgo/src/cgo.c"
+CGO_BIN="./cgo"
 
-if [ ! -f "$CGO" ]; then
-    echo "Error: $CGO not found."
-    exit 1
-fi
-
-chmod +x "$CGO"
+gcc -Os -s -fno-asynchronous-unwind-tables -o "$CGO_BIN" "$CGO_SRC"
+chmod +x "$CGO_BIN"
 
 export PREFIX="$BUILD_DIR"
 
-$CGO -w -shared -o libfrees.so native/frees/src/*.c -Inative/frees/include
-$CGO -w -o glog native/glog/src/glog.c
-$CGO -w -o gp native/gp/src/gp.c
-$CGO -w -o mgr native/mgr/src/mgr.c native/mgr/src/sdk.c -Inative/mgr/include -lcurl
-$CGO -w -o csd native/csd/src/*.c -Inative/csd/include
-$CGO -w -o kdio native/kdio/src/kdio.c
+"$CGO_BIN" -w -shared -o libfrees.so native/frees/src/*.c -Inative/frees/include
+"$CGO_BIN" -w -o glog native/glog/src/glog.c
+"$CGO_BIN" -w -o gp native/gp/src/gp.c
+"$CGO_BIN" -w -o mgr native/mgr/src/mgr.c native/mgr/src/sdk.c -Inative/mgr/include -lcurl
+"$CGO_BIN" -w -o csd native/csd/src/*.c -Inative/csd/include
+"$CGO_BIN" -w -o kdio native/kdio/src/kdio.c
 
 mv libfrees.so "$BUILD_DIR/lib/"
 mv glog gp mgr csd kdio "$BUILD_DIR/bin/"
 
-cp "$CGO" "$BUILD_DIR/bin/cgo"
+cp "$CGO_BIN" "$BUILD_DIR/bin/cgo"
+rm "$CGO_BIN"
